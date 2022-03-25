73e82851a494d6f780c76e3a47da7f62c87cb62a
Stefano <stafanoraparelli@gmail.com>
2022-03-25 10:24:50 +0100

    fast commit

--- models/User.js
+++ models/User.js
@@ -9,6 +9,11 @@ const UserSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true,
+    },
+    role:{
+        type: String,
+        required: true,
+        default: 'simple'
     }
 });
 
